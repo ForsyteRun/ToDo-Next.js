bd5bce4a2ff0a52714683ff125ffc8a73896d4c9
ForsyteRun <forsyterun@gmail.com>
2022-11-09 08:43:31 +0200

    list done

--- pages/Answer.tsx
+++ pages/Answer.tsx
@@ -1,8 +1,8 @@
-import React, { FC, useState } from 'react'
+import React, { FC, useState } from "react";
 import Checkbox from "@mui/material/Checkbox";
-import CircleChecked from "@mui/icons-material/CheckCircleOutline"
-import CircleUnchecked from "@mui/icons-material/RadioButtonUnchecked"
-import ClearIcon from '@mui/icons-material/Clear';
+import CircleChecked from "@mui/icons-material/CheckCircleOutline";
+import CircleUnchecked from "@mui/icons-material/RadioButtonUnchecked";
+import ClearIcon from "@mui/icons-material/Clear";
 import {
   IconButton,
   List,
@@ -12,21 +12,39 @@ import {
   ListItemText,
   TextField,
 } from "@mui/material";
-import { FormDataType } from '../types/type';
+import { FormDataType } from "../types/type";
 
 type PropsType = {
-  el: FormDataType
-}
+  el: FormDataType;
+};
 
-export const Answer: FC<PropsType> = ({el}) => {
-  
+export const Answer: FC<PropsType> = ({ el }) => {
   return (
-    <>
-    {
-      console.log(el)
-    }
-    </>
-    )
+    <List sx={{ width: "100%", maxWidth: 360, bgcolor: "background.paper" }}>
+      <ListItem
+        key={el.id}
+        secondaryAction={
+          <IconButton edge="end" aria-label="comments">
+            <ClearIcon />
+          </IconButton>
+        }
+        disablePadding
+      >
+        <ListItemButton role={undefined} onClick={() => console.log(111)} dense>
+          <ListItemIcon>
+            <Checkbox
+              edge="start"
+              //checked={checked.indexOf(value) !== -1}
+              tabIndex={-1}
+              disableRipple
+              inputProps={{ "aria-labelledby": String(el.id) }}
+            />
+          </ListItemIcon>
+          <ListItemText id={String(el.id)} primary={el.name} />
+        </ListItemButton>
+      </ListItem>
+    </List>
+  )
 }
 
-export default Answer
+export default Answer;
